920b4f99b3c486337a8ba3f9a1bb52bc9d01e65f
Ganesh Ghalame <ganesh.ghalame@clairvoyantsoft.com>
2018-04-20 06:46:38 +0530

    Completed Recipe Listing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  // TODO change image paths
+  recipes: Recipe[] = [
+    new  Recipe('A test Recipe', 'This is a simply test', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new  Recipe('A test Recipe', 'This is a simply test',
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+  ];
   constructor() { }
 
   ngOnInit() {
